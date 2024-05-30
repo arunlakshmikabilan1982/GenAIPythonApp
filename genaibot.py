b517bd03242ee4d6088ae0a12c042805ee05a599
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2024-05-30 11:47:21 +0530

    changed text to query
    changed text to query

--- genaibot.py
+++ genaibot.py
@@ -35,7 +35,7 @@ safety_settings = [
 @app.route('/askgenaibot', methods=['POST'])
 def askgenaibot():
     data = json.loads(request.data)
-    usertext = data['text']
+    usertext = data['Query']
     model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
