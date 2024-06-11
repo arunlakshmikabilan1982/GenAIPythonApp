09390953bc0ccf34adb5e613cc27320e7d251653
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2024-06-11 14:56:43 +0530

    changed API file names, and added seo generator code
    changed API file names, and added seo generator code

---
+++ gemini_translation_api.py
@@ -0,0 +1,24 @@
+import os
+import google.generativeai as genai
+from flask import Flask, json, request
+from environment import GEMINI_API_KEY
+
+app = Flask(__name__)
+
+# Get the API key from the environment variables
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
+
+@app.route('/aitranslator', methods=['POST']) 
+def genaitranslator():
+   data = json.loads(request.data)
+   sourcelanguage = data['sourcelanguage']
+   targetlanguage = data['targetlanguage']
+   text = data['text']
+   response = model.generate_content(f"Translate the following sentence from language {sourcelanguage} to language {targetlanguage}: {text}")
+   return response.text
+
+if __name__ == '__main__':
+    #
+   app.run(host="localhost", port=8000)   
