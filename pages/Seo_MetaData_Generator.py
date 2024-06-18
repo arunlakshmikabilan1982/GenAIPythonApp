3ca8a7133a18cfcd49b6eb1231043cce4f49cb1c
Arun Lakshmi Kabilan <121877048+arunlakshmikabilan1982@users.noreply.github.com>
2024-06-18 06:01:54 +0000

    changed for working of seo_metadata generator

--- pages/Seo_MetaData_Generator.py
+++ pages/Seo_MetaData_Generator.py
@@ -6,7 +6,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 from pinecone import Pinecone, ServerlessSpec
 from langchain.chains import ConversationalRetrievalChain
-from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
 from langchain_core.prompts import PromptTemplate
 # from environment import PINECONE_SEO_INDEX, GEMINI_API_KEY, PINECONE_API_KEY
 from PIL import Image
@@ -22,16 +22,6 @@ import openpyxl
 import tempfile
 import requests
 import io, json
-from dotenv import load_dotenv
-import os
-
-
-load_dotenv()  
-
-
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-# PINECONE_SEO_INDEX = os.getenv("PINECONE_SEO_INDEX")
 
 GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
