c1e82650fc500eecdd3e248f8cca280fcb0effeb
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2024-06-19 16:17:13 +0530

    added general query method
    added general query method

--- orgdata_gemini_api.py
+++ orgdata_gemini_api.py
@@ -1,16 +1,12 @@
-from pathlib import Path
-
-from langchain.chains import ConversationalRetrievalChain
-
 #used dependencies
+from pathlib import Path
+from langchain.chains import ConversationalRetrievalChain
 from langchain_pinecone import PineconeVectorStore
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
-import json
-from flask import Flask , jsonify
-from flask import json,request
+from flask import Flask, json, request
 app = Flask(__name__)
 
 from environment import PINECONE_INDEX, GEMINI_API_KEY
@@ -32,6 +28,42 @@ def retriever_existingdb():
     search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.5}
     )
     return retriever
+
+def query_llm(retriever, query):
+    general_system_template = r""" 
+    Given a specific context, Provide 10 seperate paragraphed Answers except for Events.
+    Do not provide paragraphed answers for Events related queries.
+    Generate content as specified for Article, Blog queries and use Article and Blog generic templates, generate Article and Blog content based on requested timeframe. do not use numbering.
+    Do not generate content for multiple events. 
+    For Events use below Template to answer, Generate More Content for one asked event and fetch event based on requested timeframe.
+    Event Title:
+    Description:
+    Date and Time:
+    Location:
+    ----
+    {context}
+    ----
+    """
+
+    general_ai_template = "role:content creator"
+    general_user_template = "Question:```{query}```"
+    messages = [
+            SystemMessagePromptTemplate.from_template(general_system_template),
+            HumanMessagePromptTemplate.from_template(general_user_template),
+            AIMessagePromptTemplate.from_template(general_ai_template)
+               ]
+    qa_prompt = ChatPromptTemplate.from_messages( messages )
+ 
+    qa = ConversationalRetrievalChain.from_llm(
+            llm=model,
+            retriever=retriever,
+            chain_type="stuff",
+            verbose=True,
+            combine_docs_chain_kwargs={'prompt': qa_prompt}
+        )
+    result = qa({"question": query, "query": query, "chat_history": ""})
+    result = result["answer"]
+    return result
 
 def contentgenerator_llm(retriever, query, contenttype, format):
 
@@ -59,56 +91,25 @@ def contentgenerator_llm(retriever, query, contenttype, format):
         )
     result = qa({"question": query, "query": query, "chat_history": ""})
     result = result["answer"]
-    print(result)
     return result
-
-class LLMResponseArticle:
-      def __init__(self, ArticleTitle, ArticleBody,ArticleHeadline,ArticleLeadParagraph,ArticleExplanation):
-       self.ArticleTitle = ArticleTitle
-       self.ArticleBody = ArticleBody
-       self.ArticleHeadline = ArticleHeadline
-       self.ArticleLeadParagraph = ArticleLeadParagraph
-       self.ArticleExplanation = ArticleExplanation
 
 @app.route('/contentgeneratorbot', methods=['POST']) 
 def contentgenerator_ai():
-
-    #   return retriever_llm(queryfromfe)
     data = request.get_json()
     print(data)
     queryfromfe = data['Query']
     contenttype = data['ContentType']
     format_type = data['FormatType']
     retriever = retriever_existingdb()
-    response = contentgenerator_llm(retriever, queryfromfe, contenttype, format_type)
-    #    print(result)
-    #    return result
-    #    articleTitle = "" ; articleBody = "" ; articleHeadline = "" ; articleLeadParagraph = "" ; articleExplanation = ""
-
-    #    if contenttype == "article":
-    #         if format == "template1":
-    #           articleTitle = result.split("ArticleBody--")[0]
-    #           articleBody = result.split("ArticleBody--")[1]
-    #         if format == "template2":
-    #           articleLeadParagraphSplit = result.split("ArticleLeadParagraph--")
-    #           articleExplanationSplit = articleLeadParagraphSplit[1].split("ArticleExplanation--")
-    #           articleHeadline = articleLeadParagraphSplit[0]
-    #           articleLeadParagraph = articleExplanationSplit[0]
-    #           articleExplanation = articleExplanationSplit[1]
-            
-    #         response = jsonify({"ArticleTitle":articleTitle,"ArticleBody":articleBody,"ArticleHeadline":articleHeadline,
-    #                             "ArticleLeadParagraph":articleLeadParagraph,"ArticleExplanation":articleExplanation})
-    #         return response
-    #    if contenttype == "blog":
-    #         response = jsonify({"ArticleTitle":articleTitle,"ArticleBody":articleBody,"ArticleHeadline":articleHeadline,
-    #                             "ArticleLeadParagraph":articleLeadParagraph,"ArticleExplanation":articleExplanation})
+    response = contentgenerator_llm(retriever, queryfromfe, contenttype.lower(), format_type.lower())
     return response
 
-# @app.route('/askaibot/<queryfromfe>') 
-# def query_ai(queryfromfe):
-#     #   return retriever_llm(queryfromfe)
-#     #    retriever = retriever_existingdb()
-    #    return query_llm(retriever, queryfromfe)
+@app.route('/askaibot', methods=['POST'])
+def query_ai():
+       data = request.get_json()
+       queryfromfe = data['query']
+       retriever = retriever_existingdb()
+       return query_llm(retriever, queryfromfe)
 
 if __name__ == '__main__':
     #
